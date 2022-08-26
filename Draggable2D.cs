6bad91f87cd8053f3c545c170b8df2d48a37ded0
Hank <terriblehank@outlook.com>
2022-08-27 06:53:22 +0800

    Add files via upload

--- Draggable2D.cs
+++ Draggable2D.cs
@@ -18,18 +18,29 @@ public class Draggable2D : MonoBehaviour
     Callback mouseUp;
 
     public TYPE type = TYPE.FREE;
-    public bool repositionOnRelease;    //松开鼠标时是否返回初始位置
 
     public bool limiting;   //是否限制拖拽的范围
     public Vector2 limitRectLeftBottom; //范围限制矩形左下角 负负
     Vector2 LRLB { get { return limitRectLeftBottom + new Vector2(originPos.x, originPos.y); } }
     public Vector2 limitRectRightTop;   //范围限制矩形右下角 正正
     Vector2 LRRT { get { return limitRectRightTop + new Vector2(originPos.x, originPos.y); } }
-    public bool dragging;   //是否正在拖拽，鼠标点击并且移动后才会判定为拖拽中
+    public Vector3 originPos;  //transform初始位置
 
-    Vector3 originPos;  //transform初始位置
+    [SerializeField]
+    bool clicked;
+    public bool Clicked { get { return clicked; } }
 
-    bool isVertical;    //此参数仅在type为CROSS时启用，判定此次拖拽的方向
+    [SerializeField]
+    bool dragging;   //是否正在拖拽，鼠标点击并且移动后才会判定为拖拽中
+    public bool Dragging{ get { return dragging; } }
+
+    [SerializeField]
+    private bool isVertical;
+    public bool IsVertical { get { return isVertical; } }    //此参数仅在type为CROSS时启用，判定此次拖拽的方向
+
+    [SerializeField]
+    bool aborted;
+    public bool Aborted { get { return aborted; } }
 
     // Start is called before the first frame update
     void Start()
@@ -40,7 +51,7 @@ public class Draggable2D : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 
     public void SetMouseDownCallback(Callback c)
@@ -58,6 +69,7 @@ public class Draggable2D : MonoBehaviour
 
     private void OnMouseDown()
     {
+        clicked = true;
         clickPos = MousePos();
         Vector3 mousePosition = MousePos();
         positionOffset = mousePosition - transform.position;
@@ -66,6 +78,8 @@ public class Draggable2D : MonoBehaviour
 
     private void OnMouseDrag()
     {
+        if (Aborted == true) return;
+
         Vector3 mousePosition = MousePos();
 
         if (!dragging)//进入了拖拽事件但鼠标位置还没有开始移动的状态
@@ -87,12 +101,10 @@ public class Draggable2D : MonoBehaviour
 
     private void OnMouseUp()
     {
-        if (repositionOnRelease)
-        {
-            transform.position = originPos;
-        }
-        dragging = false;
         if (mouseUp != null) mouseUp.Invoke();
+        dragging = false;
+        clicked = false;
+        aborted = false;
     }
 
     Vector3 MousePos()
@@ -107,7 +119,7 @@ public class Draggable2D : MonoBehaviour
         TYPE temp = type;
         if (temp == TYPE.CROSS)
         {
-            temp = isVertical ? TYPE.VERTICAL : TYPE.HORIZONTAL;
+            temp = IsVertical ? TYPE.VERTICAL : TYPE.HORIZONTAL;
         }
 
         switch (temp)
@@ -135,6 +147,11 @@ public class Draggable2D : MonoBehaviour
                 break;
         }
         return pos;
+    }
+
+    public void StopDragging()
+    {
+        if (clicked) aborted = true;      
     }
 }
 
